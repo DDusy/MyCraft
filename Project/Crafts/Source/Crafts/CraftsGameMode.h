27568b408a5a5c1cae45ec52838bf39ba708f4b1
DDusy <dkdnb50@gmail.com>
2022-03-20 04:40:13 +0900

    Server Test

---
+++ Project/Crafts/Source/Crafts/CraftsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CraftsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACraftsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACraftsGameMode();
+};
+
+
+
